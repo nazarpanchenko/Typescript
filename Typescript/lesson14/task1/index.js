a66256d4bb5960a519be12ce28f3a1f8d451c22e
Nazar <panchenko.nazar.a@gmail.com>
2020-03-20 12:53:25 +0200

    edit

--- Typescript/lesson14/task1/index.js
+++ Typescript/lesson14/task1/index.js
@@ -1,19 +1,17 @@
 'use strict';
 
-let message = '';
+let message = 'Just learn it!';
+
+export function sendMessage (name) {
+    let sender = 'Gromcode';
+
+    console.log(`${name}, ${message} Your ${sender}`);
+}
 
 export function setMessage(text) {
     message = text;
 }
 
-export function sendMessage (name) {
-    let sender = 'Gromcode';
-    
-    console.log(`${name}, ${message} Your ${sender}`);
-}
-
-setMessage('Just learn it!');
-sendMessage('Ann');
-
 setMessage('Good job!');
+
 sendMessage('Ann');
