3c0011fa1e615149d9d736af7b23cab0fe875e2b
Nazar <panchenko.nazar.a@gmail.com>
2020-03-20 12:38:41 +0200

    Edit

--- Typescript/lesson14/task1/index.js
+++ Typescript/lesson14/task1/index.js
@@ -5,10 +5,12 @@ let message = '',
 
 export function setMessage(text) {
     message = text;
+
+    return message;
 }
 
 export function sendMessage (name) {
-    console.log(`${name}, ${message} Your ${sender}`);
+    console.log(`${name}, ${setMessage(message)} Your ${sender}`);
 }
 
 setMessage('Just learn it!');
