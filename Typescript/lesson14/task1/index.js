e52b0ef2a053a061f2a62fc6045ecda73853ae00
Nazar <panchenko.nazar.a@gmail.com>
2020-03-24 14:38:20 +0200

    Edit

--- Typescript/lesson14/task1/index.js
+++ Typescript/lesson14/task1/index.js
@@ -1,12 +1,11 @@
 let message = 'Just learn it!';
 
-export function setMessage(text) {
-    message = text;
-}
-
 export function sendMessage (name) {
     const sender = 'Gromcode';
 
     console.log(`${name}, ${message} Your ${sender}`);
-    console.log(`${name}, ${setMessage('Good job!')} Your ${sender}`);
+}
+
+export function setMessage(text) {
+    message = text;
 }
---
+++ Typescript/lesson14/task1/index.test.js
@@ -0,0 +1,9 @@
+import { sendMessage, setMessage } from './index';
+
+it ('should check if message is correct', () => {
+    sendMessage('Ann');
+});
+
+it ('should check if new message is set correctly', () => {
+    setMessage('Good job!');
+});
