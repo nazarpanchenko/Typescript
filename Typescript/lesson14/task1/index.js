bd0f2d8016319b8ada899bc0a238f1b068e1e4cb
Nazar <panchenko.nazar.a@gmail.com>
2020-03-24 14:11:17 +0200

    Edit

--- Typescript/lesson14/task1/index.js
+++ Typescript/lesson14/task1/index.js
@@ -1,12 +1,13 @@
 let message = 'Just learn it!';
 
+export function setMessage(text) {
+    message = text;
+}
+
+setMessage('Good job!');
+
 export function sendMessage (name) {
     const sender = 'Gromcode';
 
     console.log(`${name}, ${message} Your ${sender}`);
 }
-
-export function setMessage(text) {
-    message = text;
-}
-
