1ad63a53af8ad31938b2e1275c644f8c3302b949
Nazar <panchenko.nazar.a@gmail.com>
2020-03-26 20:30:51 +0200

    Edit

--- Typescript/lesson15/task1/index.test.js
+++ Typescript/lesson15/task1/index.test.js
@@ -1,13 +1,9 @@
 import { createCalculator } from './index';
 
-it('should check the value of the first counter', () => {
-    const calculator = createCalculator().addMemory(8);
+it('should check if counters are independent', () => {
+    const calculator1 = createCalculator().addMemory(8);
+    const calculator2 = createCalculator().decreaseMemory(3);
 
-    expect(calculator).toEqual(8);
-});
-
-it('should check the value of the second counter', () => {
-    const calculator = createCalculator().decreaseMemory(3);
-
-    expect(calculator).toEqual(-3);
+    expect(calculator1).toEqual(8);
+    expect(calculator2).toEqual(-3);
 });
