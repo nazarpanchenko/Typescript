9bbc6d4df49dc1718d884f3cd09edd84a8f9de86
Nazar <panchenko.nazar.a@gmail.com>
2020-03-26 17:39:45 +0200

    Edit

--- Typescript/lesson15/task1/index.js
+++ Typescript/lesson15/task1/index.js
@@ -1,25 +1,27 @@
-let memory = 0;
-
 export const createCalculator = () => {
     function add(value) {
+        let memory = 0;
         memory += value;
 
         return memory;
     }
 
     function decrease(value) {
+        let memory = 0;
         memory -= value;
 
         return memory;
     }
 
     function reset() {
-        memory = 0;
+        let memory = 0;
 
         return memory;
     }
 
     function getMemo() {
+        let memory = 0;
+        
         return memory;
     }
 
