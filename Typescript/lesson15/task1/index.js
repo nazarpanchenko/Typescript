46f90865c6fe562bf59d0628d3b8a943c0953d07
Nazar <panchenko.nazar.a@gmail.com>
2020-03-26 19:21:10 +0200

    edit

--- Typescript/lesson15/task1/index.js
+++ Typescript/lesson15/task1/index.js
@@ -4,20 +4,15 @@ export const createCalculator = () => {
     const add = (value) => {
         let memory = 0;
         memory += value;
-
-        return memory;
     };
 
     const decrease = (value) => {
         let memory = 0;
         memory -= value;
-
-        return memory;
     };
 
     const reset = () => {
         memory = 0;
-        return memory;
     };
 
     const getMemo = () => {
