39ff96f076b13cb6319dbabf3488b471ce75be88
Nazar <panchenko.nazar.a@gmail.com>
2020-03-26 18:27:47 +0200

    Edit

--- Typescript/lesson15/task2/index.js
+++ Typescript/lesson15/task2/index.js
@@ -1,10 +1,7 @@
-const makeCounter = () => {
+export const makeCounter = () => {
     let count = 0;
-    
+
     return function() {
         return count++;
     }
 };
-
-const counter1 = makeCounter();
-const counter2 = makeCounter();
--- Typescript/lesson15/task2/index.test.js
+++ Typescript/lesson15/task2/index.test.js
@@ -1,1 +1,14 @@
+import { makeCounter } from './index';
 
+it('should check if the counters are independent', () => {
+    const counter1 = makeCounter();
+    const counter2 = makeCounter();
+
+    counter1();
+    counter1();
+    counter2();
+    counter1();
+    counter2();
+
+    expect(counter1).toEqual(2);
+});
