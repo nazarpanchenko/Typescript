3b7814efe01d4a58de13eb862c02995de36fc411
Nazar <panchenko.nazar.a@gmail.com>
2020-03-27 17:04:59 +0200

    Edit

---
+++ Typescript/lesson15/task3/index.js
@@ -0,0 +1,50 @@
+export const createLogger = () => {
+    let data = {
+        message : '',
+        dateTime : null,
+        type : ''
+    };
+    let logs = [];
+
+    const warn = (warnMessage) => {
+        data.message = warnMessage;
+        data.dateTime = new Date();
+        data.type = 'warn';
+        logs.push(data);
+
+        return logs;
+    };
+
+    const error = (errMessage) => {
+        data.message = errMessage;
+        data.dateTime = new Date();
+        data.type = 'error';
+        logs.push(data);
+
+        return logs;
+    };
+
+    const log = (logMessage) => {
+        data.message = logMessage;
+        data.dateTime = new Date();
+        data.type = 'log';
+        logs.push(data);
+
+        return logs;
+    };
+
+    const getRecords = (logType) => {
+        if (!logType) return logs;
+
+        if (logType !== 'warn' && logType !== 'err' && logType !== 'log') return [];
+
+        return logs.sort((a, b) => a.dateTime < b.dateTime ? 1 : -1);
+    };
+
+    return {
+        warn : warn,
+        error : error,
+        log : log,
+        getRecords : getRecords
+    };
+};
---
+++ Typescript/lesson15/task3/index.test.js
@@ -0,0 +1,12 @@
+import { createLogger } from './index';
+
+it('should check if all counters are independent', () => {
+    const counter1 = createLogger().warn('You got a warning message!');
+    const counter2 = createLogger().error('You got an error!');
+    const counter3 = createLogger().warn('New log');
+
+    expect(counter1).toEqual(['You got a warning message!']);
+    expect(counter2).toEqual(['You got an error!!']);
+    expect(counter3).toEqual(['New log']);
+    expect(counter1).toEqual(['You got a warning message!']);
+});
