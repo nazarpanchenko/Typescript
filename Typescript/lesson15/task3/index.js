a85bdb92f1eafd745c1c401340eab9ab3e3aff3e
Nazar <panchenko.nazar.a@gmail.com>
2020-03-28 17:10:50 +0200

    Edit

--- Typescript/lesson15/task3/index.js
+++ Typescript/lesson15/task3/index.js
@@ -36,6 +36,8 @@ export const createLogger = () => {
     const getRecords = (logType) => {
         if (!logType) return logs.sort((a, b) => a.dateTime > b.dateTime ? 1 : -1);
 
+        logs = logs.filter(data => data.type == logType);
+
         return logs.sort((a, b) => a.dateTime > b.dateTime ? 1 : -1);;
     };
 
--- Typescript/lesson15/task3/index.test.js
+++ Typescript/lesson15/task3/index.test.js
@@ -4,10 +4,11 @@ it('should check if all counters are independent', () => {
     const counter1 = createLogger().warn('You got a warning message!');
     const counter2 = createLogger().error('You got an error!');
     const counter3 = createLogger().warn('New log');
-    const getter = createLogger().getRecords('warn');
+    const getLog = createLogger().getRecords('warn');
 
     expect(counter1).toEqual(['You got a warning message!']);
-    expect(counter2).toEqual(['You got an error!!']);
+    expect(counter2).toEqual(['You got an error!']);
     expect(counter3).toEqual(['New log']);
     expect(counter1).toEqual(['You got a warning message!']);
+    expect(getLog).toEqual(['warn']);
 });
