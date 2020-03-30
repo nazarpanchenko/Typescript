dd9e6eaf2cde05117d9056ddbc85a7b8d9f265e4
Nazar <panchenko.nazar.a@gmail.com>
2020-03-30 13:34:56 +0300

    Edit

--- Typescript/lesson15/task3/index.js
+++ Typescript/lesson15/task3/index.js
@@ -34,9 +34,9 @@ export const createLogger = () => {
     };
 
     const getRecords = (logType) => {
-        if (!logType) return logs.sort((a, b) => a.dateTime - b.dateTime);
+        if (!logType) return logs.sort((a, b) => b.dateTime < a.dateTime ? -1 : 1);
 
-        return logs.filter(data => data.type === logType).sort((a, b) => a.dateTime - b.dateTime);
+        return logs.filter(data => data.type === logType).sort((a, b) => b.dateTime < a.dateTime ? -1 : 1);
     };
 
     return {
