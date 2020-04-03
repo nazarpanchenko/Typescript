41d31da536773bf87c86ffbb9069aa00ad42639b
Nazar <panchenko.nazar.a@gmail.com>
2020-04-03 09:49:29 +0300

    Edit

--- Typescript/lesson15/task3/index.js
+++ Typescript/lesson15/task3/index.js
@@ -1,36 +1,32 @@
 export const createLogger = () => {
     let logs = {
-        message: '...',
+        message: [],
         dateTime: new Date(),
         type: '...'
     };
-    let records = [];
 
     const warn = (warnMessage) => {
-        logs.message = warnMessage;
+        logs.message.push(warnMessage);
         logs.dateTime = new Date();
         logs.type = 'warn';
-        records.push(logs);
     };
 
     const error = (errMessage) => {
-        logs.message = errMessage;
+        logs.message.push(errMessage);
         logs.dateTime = new Date();
         logs.type = 'error';
-        records.push(logs);
     };
 
     const log = (logMessage) => {
-        logs.message = logMessage;
+        logs.message.push(logMessage);
         logs.dateTime = new Date();
         logs.type = 'log';
-        records.push(logs);
     };
 
     const getRecords = (logType) => {
-        if (!logType) return records.sort((a, b) => b.dateTime > a.dateTime ? -1 : 1);
+        if (!logType) return logs.message.sort((a, b) => b.dateTime > a.dateTime ? -1 : 1);
 
-        return records.filter(logs => logs.type === logType).sort((a, b) => b.dateTime > a.dateTime ? -1 : 1);
+        return records.filter(message => message.type === logType).sort((a, b) => b.dateTime > a.dateTime ? -1 : 1);
     };
 
     return {
