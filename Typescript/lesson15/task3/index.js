5173b7d8afa17fe08b8aa200e2278252ba4abe78
Nazar <panchenko.nazar.a@gmail.com>
2020-04-03 18:19:30 +0300

    Edit

--- Typescript/lesson15/task3/index.js
+++ Typescript/lesson15/task3/index.js
@@ -1,32 +1,35 @@
 export const createLogger = () => {
     let logs = {
         message: [],
-        dateTime: new Date(),
-        type: '...'
+        dateTime: [],
+        type: []
     };
 
     const warn = (warnMessage) => {
         logs.message.push(warnMessage);
-        logs.dateTime = new Date();
-        logs.type = 'warn';
+        logs.dateTime.push(new Date());
+        logs.type.push('warn');
     };
 
     const error = (errMessage) => {
         logs.message.push(errMessage);
-        logs.dateTime = new Date();
-        logs.type = 'error';
+        logs.dateTime.push(new Date());
+        logs.type.push('error');
     };
 
     const log = (logMessage) => {
         logs.message.push(logMessage);
-        logs.dateTime = new Date();
-        logs.type = 'log';
+        logs.dateTime.push(new Date());
+        logs.type.push('log');
     };
 
     const getRecords = (logType) => {
-        if (!logType) return logs.message.sort((a, b) => b.dateTime > a.dateTime ? -1 : 1);
+        if (!logType) return logs.dateTime.sort((a, b) => b > a ? -1 : 1);
 
-        return logs.message.filter(message => message === logType).sort((a, b) => b.dateTime > a.dateTime ? -1 : 1);
+        logs.type = logs.type.filter(log => log === logType);
+        logs.dateTime = logs.dateTime.sort((a, b) => b > a ? -1 : 1);
+
+        return logs.message;
     };
 
     return {
