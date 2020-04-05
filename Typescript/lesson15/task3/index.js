0c0d4f41575ca8a6e5acc536c2a111ef80580f81
Nazar <panchenko.nazar.a@gmail.com>
2020-04-05 13:13:31 +0300

    Edit

--- Typescript/lesson15/task3/index.js
+++ Typescript/lesson15/task3/index.js
@@ -1,59 +1,35 @@
 export const createLogger = () => {
     let logs = {
-        message: '',
-        dateTime: null,
-        type: ''
+        message: [],
+        dateTime: [],
+        type: []
     };
-    let records = [];
 
     const warn = (warnMessage) => {
-        logs.message = warnMessage;
-        logs.dateTime = new Date();
-        logs.type = 'warn';
-        records.push(logs);
-
-        return records;
+        logs.message.push(warnMessage);
+        logs.dateTime.push(new Date());
+        logs.type.push('warn');
     };
 
     const error = (errMessage) => {
-        logs.message = errMessage;
-        logs.dateTime = new Date();
-        logs.type = 'error';
-        records.push(logs);
-
-        return records;
+        logs.message.push(errMessage);
+        logs.dateTime.push(new Date());
+        logs.type.push('error');
     };
 
     const log = (logMessage) => {
-        logs.message = logMessage;
-        logs.dateTime = new Date();
-        logs.type = 'log';
-        records.push(logs);
-
-        return records;
+        logs.message.push(logMessage);
+        logs.dateTime.push(new Date());
+        logs.type.push('log');
     };
 
     const getRecords = (logType) => {
-        let result = [];
-        
-        if (!logType) {
-            records = records.sort((log_A, log_B) => log_B.dateTime > log_A.dateTime ? -1 : 1);
+        if (!logType) return logs.dateTime.sort((a, b) => b > a ? -1 : 1);
 
-            for (let i = 0; i < records.length; i++) {
-                result.push(records[i].message);
-            }    
+        logs.type = logs.type.filter(log => log === logType);
+        logs.dateTime = logs.dateTime.sort((a, b) => b > a ? -1 : 1);
 
-            return result;
-        }
-
-        records = records.filter(log => log.type === logType);
-        records = records.sort((log_A, log_B) => log_B.dateTime > log_A.dateTime ? -1 : 1);
-
-        for (let i = 0; i < records.length; i++) {
-            result.push(records[i].message);
-        }
-
-        return result;
+        return logs.message;
     };
 
     return {
