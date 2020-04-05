ea229aea4ea7d8e7f93aab6d88e1a3d9a10e2711
Nazar <panchenko.nazar.a@gmail.com>
2020-04-05 15:10:33 +0300

    Edit

--- Typescript/lesson15/task3/index.js
+++ Typescript/lesson15/task3/index.js
@@ -1,40 +1,36 @@
 export const createLogger = () => {
     let logs = {
-        message : [],
-        dateTime : [],
-        type : []
+        message : '',
+        dateTime : null,
+        type : ''
     };
+    let records = [];
 
     const warn = (warnMessage) => {
-        logs.message.push(warnMessage);
-        logs.dateTime.push(new Date());
-        logs.type.push('warn');
+        logs.message = warnMessage;
+        logs.dateTime = new Date();
+        logs.type = 'warn';
+        records.push(logs);
     };
 
     const error = (errMessage) => {
-        logs.message.push(errMessage);
-        logs.dateTime.push(new Date());
-        logs.type.push('error');
+        logs.message = errMessage;
+        logs.dateTime = new Date();
+        logs.type = 'error';
+        records.push(logs);
     };
 
     const log = (logMessage) => {
-        logs.message.push(logMessage);
-        logs.dateTime.push(new Date());
-        logs.type.push('log');
+        logs.message = warnMessage;
+        logs.dateTime = new Date();
+        logs.type = 'log';
+        records.push(logs);
     };
 
     const getRecords = (logType) => {
-        if (!logType) {
-            logs = logs.dateTime.sort((a, b) => b > a ? -1 : 1);
+        if (!logType) return records.sort((a, b) => b.dateTime > a.dateTime ? -1 : 1);
 
-            return logs.message;
-
-        }
-
-        logs.dateTime = logs.dateTime.sort((a, b) => b > a ? -1 : 1);
-        logs.message = message.filter(log => log.type === logType);
-
-        return logs.message;
+        return records.filter(log => log.type === logType).sort((a, b) => b.dateTime > a.dateTime ? -1 : 1);
     };
 
     return {
