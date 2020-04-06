6d048e689499d224f3581f80286628cc4f8a1fa8
Nazar <panchenko.nazar.a@gmail.com>
2020-04-06 17:01:07 +0300

    Edit

--- Typescript/lesson15/task3/index.js
+++ Typescript/lesson15/task3/index.js
@@ -1,39 +1,37 @@
 export const createLogger = () => {
-    let records = {
-        message : [],
-        dateTime : [],
-        type : []
+    const records = {
+        warn: [],
+        error: [],
+        log: []
     };
-
-    const warn = (warnMessage) => {
-        records.message.push(warnMessage);
-        records.dateTime.push(new Date());
-        records.type.push('warn');
-    };
-
-    const error = (errMessage) => {
-        records.message.push(errMessage);
-        records.dateTime.push(new Date());
-        records.type.push('error');
-    };
-
-    const log = (logMessage) => {
-        records.message.push(logMessage);
-        records.dateTime.push(new Date());
-        records.type.push('log');
-    };
-
-    const getRecords = (type) => {
-        return type ? records[type].reverse()
-            : Object.values(records)
-                .reduce((acc, rec) => acc.concat(rec), [])
-                .sort((a, b) => (b.dateTime < a.dateTime ? -1 : 1));
-    };
-
     return {
-        warn : warn,
-        error : error,
-        log : log,
-        getRecords : getRecords
-    };
+        warn(message) {
+            records.warn.push({
+                message,
+                dateTime: new Date(),
+                type: 'warn'
+            });
+        },
+        error(message) {
+            records.error.push({
+                message,
+                dateTime: new Date(),
+                type: 'error'
+            });
+        },
+        log(message) {
+            records.log.push({
+                message,
+                dateTime: new Date(),
+                type: 'log'
+            });
+        },
+        getRecords(type) {
+            return type
+                ? records[type].reverse()
+                : Object.values(records)
+                    .reduce((acc, rec) => acc.concat(rec), [])
+                    .sort((a, b) => (b.dateTime < a.dateTime ? -1 : 1));
+        }
+    };  
 };
