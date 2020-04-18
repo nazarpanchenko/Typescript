b2ffafdccc9076018afa914388b8047099736870
Nazar <panchenko.nazar.a@gmail.com>
2020-04-18 15:18:07 +0300

    Edit

--- Typescript/lesson16/task3/index.js
+++ Typescript/lesson16/task3/index.js
@@ -1,7 +1,7 @@
 export const createArrayOfFunctions = (arrLength) => {
     if (isNaN(arrLength)) return null;  
 
-    if (arrLength == undefined) return [];
+    if (!!arrLength) return [];
     
     let arr = [];
 
