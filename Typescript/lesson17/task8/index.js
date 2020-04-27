19c484da871d6fb243f0f7a64410b5b8f9f46e61
Nazar <panchenko.nazar.a@gmail.com>
2020-04-27 21:47:06 +0300

    Edit

--- Typescript/lesson17/task8/index.js
+++ Typescript/lesson17/task8/index.js
@@ -1,18 +1,17 @@
-let timerID = setInterval(() => {
-    if (this.secondsPassed == 60) {
-        this.secondsPassed = 0;
-        this.minsPassed += 1;
-    }
-
-    this.secondsPassed += 5;
-}, 5000);
-
 export const timer = {
     secondsPassed : 0,
     minsPassed : 0,
-
+    
+    timerID : setInterval(() => {
+        if (this.secondsPassed == 60) {
+            this.secondsPassed = 0;
+            this.minsPassed += 1;
+        }
+    
+        this.secondsPassed += 5;
+    }, 5000),
     startTimer : function() {
-        return timerID;
+        return this.timerID;
     },
     getTime : function() {
         if (this.secondsPassed < 10) {
