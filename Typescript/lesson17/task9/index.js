1d1dae2e2b7ee33a615068f42d3d9b795b96afbc
Nazar <panchenko.nazar.a@gmail.com>
2020-04-29 21:50:31 +0300

    Edit

--- Typescript/lesson17/task9/index.js
+++ Typescript/lesson17/task9/index.js
@@ -20,7 +20,7 @@ export function calculator(a, b) {
  * и должна принимать 2 числа и возвращать из произведение
  */
 // ...your code here
-export const multiplier = calculator.bind(a, b);
+export const multiplier = () => calculator.bind(null, a, b);
 
 /*
  * Ф-ция summator должна быть создана на основе calculator
@@ -28,11 +28,11 @@ export const multiplier = calculator.bind(a, b);
  * и должна принимать 2 числа и возвращать из сумму
  */
 // ...your code here
-export const summator = calculator.bind(a, b);
+export const summator = () => calculator.bind(null, a, b);
 /*
  * Ф-ция twice должна быть создана на основе calculator
  * с использования .bind
  * и должна принимать 1 число и возвращать это число умноженное на 2
  */
 // ...your code here
-export const twice = calculator.bind(num);
+export const twice = () => calculator.bind(null, num);
