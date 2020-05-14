463bde494a5d03f39f10ff26d89f94f3aa6eb802
Nazar <panchenko.nazar.a@gmail.com>
2020-05-14 22:25:59 +0300

    Edit

--- Typescript/lesson18/task1/index.js
+++ Typescript/lesson18/task1/index.js
@@ -3,12 +3,12 @@ export const event = {
         { name : 'Tom', email: 't@gmail.com', age : 17 },
         { name : 'Bob', email : 'b@gmail.com', age : 18 }
     ],
-    message : 'Welcome to the party',
+    message : 'Welcome to the party!',
     getInvitations() {
         return this.guests
             .filter(({ age }) => age >= 18)
             .map(({ name, email }) => ({
-                text: `Dear ${name}. ${this.message}`,
+                text: `Dear ${name}! ${this.message}`,
                 email
             }));
     }
