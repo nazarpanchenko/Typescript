c4998a3ddd3e1d1c72cb4ceec3bd896b261478ce
Nazar <panchenko.nazar.a@gmail.com>
2020-05-18 18:15:55 +0300

    Edit

--- Typescript/lesson18/task3/index.js
+++ Typescript/lesson18/task3/index.js
@@ -1,6 +1,6 @@
 export const sumOfSquares = () => {
     return [].map.call(arguments, num => num * num)
-        .reduce((acc, num) => acc + num, 0 );
+        .reduce((acc, num) => acc + num, 0);
 };
 
 sumOfSquares(1, 2, 3, 4);
