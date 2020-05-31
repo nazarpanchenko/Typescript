9cd2b3119f528eb7b49fb3be94e0ad8f8c97e884
Nazar <panchenko.nazar.a@gmail.com>
2020-05-31 16:13:47 +0300

    Edit

--- Typescript/lesson19/task4/index.js
+++ Typescript/lesson19/task4/index.js
@@ -14,7 +14,11 @@ User.prototype.requestNewPhoto = function() {
 User.prototype.setAge = function() {
     return this.age < 0 ? false :
         this.age >= 25 ? 
-            console.log(`New photo request was sent for ${this.name}`) : this.age = age;
+           () => {
+                this.age = age;
+                console.log(`New photo request was sent for ${this.name}`);
+           } : 
+                this.age = age;
 }
 
 const user = new User('Tom', 17);
