fe7084895f184c4df1462dfcf38cd956b85411f9
Nazar <panchenko.nazar.a@gmail.com>
2020-06-01 12:14:28 +0300

    Edit

--- Typescript/lesson19/task2/index.js
+++ Typescript/lesson19/task2/index.js
@@ -2,21 +2,35 @@ export const getOwnProps = (obj) => {
    let props = [];
 
    for (let prop in obj) {
-      if (obj.hasOwnProperty(prop)) props.push(prop);
+      if (obj.hasOwnProperty(prop)) {
+         props.push(prop);
+      }
    }
 
    return props;
 };
 
-let ship = {
-   hasWheels : false,
-   isMoving : true,
-   liftAnchorUp : function() {
-
+const vehicle = {
+   isMoving : false,
+   move() {
+      console.log(`${this.name} moving`);
    },
-   liftAnchorDown : function() {
-
+   stop() {
+      console.log(`${this.name} stopped`);
    }
 };
+
+const ship = {
+   name : 'Aurora',
+   hasWheels : false,
+   liftAnchorUp : function() {
+      console.log('Lifting anchor up');
+   },
+   liftAnchorDown : function() {
+      console.log('Lifting anchor down');
+   }
+};
+
+Object.setPrototypeOf(ship, vehicle);
 
 getOwnProps(ship);
