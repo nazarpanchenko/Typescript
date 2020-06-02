8258538d6a67ee76f5ee7828bf7bf3e785c7ade4
Nazar <panchenko.nazar.a@gmail.com>
2020-06-02 14:58:16 +0300

    Edit

--- Typescript/lesson19/task2/index.js
+++ Typescript/lesson19/task2/index.js
@@ -1,10 +1,14 @@
-export const getOwnProps = obj => {
+const getOwnProps = obj => {
    const result = [];
+
    for(let prop in obj) {
        if (obj.hasOwnProperty(prop)) {
            result.push(prop);
        }
    }
+
    return result
        .filter(prop => typeof obj[prop] !== 'function');
 };
+
+export const getOwnProps;
