7db98c10723914e40f9856a7af7fba5be2f6ef70
Nazar <panchenko.nazar.a@gmail.com>
2020-06-09 15:48:08 +0300

    Edit

--- Typescript/lesson20/task5/index.js
+++ Typescript/lesson20/task5/index.js
@@ -1,9 +1,9 @@
-class Order {
+export class Order {
     constructor(price, city, type) {
 
     }
 
-    id = +Math.random();
+    id = '';
     dateCreated = new Date();
     isConfirmed = false;
     dateConfirmed = new Date();
@@ -14,10 +14,12 @@ class Order {
 
     confirmOrder() {
         this.isConfirmed = true;
-        this.dateConfirmed = new Date();
+        this.dateConfirmed = this.dateCreated;
     }
 
     isValidType() {
         return this.type === 'Buy'|| this.type === 'Sell' ? true : false;
     }
 }  
+
+const order = new Order(1000, 'London', 'Buy');
