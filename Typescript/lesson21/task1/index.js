020649b7f1d86236ac7400f6bc210749c0fccccd
Nazar <panchenko.nazar.a@gmail.com>
2020-06-16 23:45:44 +0300

    Edit

--- Typescript/lesson21/task1/index.js
+++ Typescript/lesson21/task1/index.js
@@ -3,19 +3,28 @@ const tasks = [
     { text: 'Pick up Tom from airport', done: false },
     { text: 'Visit party', done: false },
     { text: 'Visit doctor', done: true },
-    { text: 'Buy meat', done: true },
+    { text: 'Buy meat', done: true }
 ];
 
 const renderListItems = listItems => {
     const listElem = document.querySelector('.list');
 
-    const listItemsElems = listItems.map(itemText => {
+    const listItemsElems = listItems
+        .sort((a, b) => a.done - b.done)
+        .map(({ text, done }) => {
         const listItemElem = document.createElement('li');
         listItemElem.classList.add('list__item');
+
         const checkboxElem = document.createElement('input');
         checkboxElem.setAttribute('type', 'checkbox');
+        checkboxElem.checked = done;
+
+        if (done) {
+            checkboxItemElem.classList.add('list__item_done');
+        }
+        
         checkboxElem.classList.add('list-item__checkbox');
-        listItemElem.append(checkboxElem, itemText);
+        listItemElem.append(checkboxElem, text);
 
         return listItemElem;
     });
