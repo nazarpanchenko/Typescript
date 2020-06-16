3d3561c41fd7d460f376673eefde9659204689c5
Nazar <panchenko.nazar.a@gmail.com>
2020-06-17 00:26:06 +0300

    Edit

--- Typescript/lesson21/task7/index.js
+++ Typescript/lesson21/task7/index.js
@@ -1,13 +1,5 @@
-export const getTitleElement = () => {
-    const titleElem = document.querySelector('.title');
-    console.dir(titleElem);
-
-    return titleElem;
-};
-
-export const getInputElement = () => {
-    const inputElem = document.querySelector('input[type="text"]');
-    console.dir(inputElem);
-
-    return inputElem;
+export const clearList = () => {
+    const elem = document.querySelector('.categories');
+    
+    elem.innerHTML = '';
 };
