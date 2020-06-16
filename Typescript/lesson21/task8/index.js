071f7b6ca14f6646243f0653c78de1ab7a388113
Nazar <panchenko.nazar.a@gmail.com>
2020-06-17 00:32:26 +0300

    Edit

--- Typescript/lesson21/task8/index.js
+++ Typescript/lesson21/task8/index.js
@@ -1,13 +1,6 @@
-export const getTitleElement = () => {
-    const titleElem = document.querySelector('.title');
-    console.dir(titleElem);
-
-    return titleElem;
-};
-
-export const getInputElement = () => {
-    const inputElem = document.querySelector('input[type="text"]');
-    console.dir(inputElem);
-
-    return inputElem;
+export const createButton = (buttonText) => {
+    let button = document.createElement('button');
+    let body = document.querySelector('body');
+    body.append(button);
+    button.innerHTML = buttonText;
 };
