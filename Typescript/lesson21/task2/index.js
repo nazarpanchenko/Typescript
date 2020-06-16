d902347ad02e3762ad5a85d4a1d4bb36d5401669
Nazar <panchenko.nazar.a@gmail.com>
2020-06-17 00:01:30 +0300

    Edit

---
+++ Typescript/lesson21/task2/index.js
@@ -0,0 +1,13 @@
+export const getTitleElement = () => {
+    const titleElem = document.querySelector('.title');
+    console.dir(titleElem);
+
+    return titleElem;
+};
+
+export const getInputElement = () => {
+    const inputElem = document.querySelector('input[type="text"]');
+    console.dir(inputElem);
+
+    return inputElem;
+};
