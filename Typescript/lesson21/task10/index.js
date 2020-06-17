33170078eba67b62718973bf1985c5447aafcbab
Nazar <panchenko.nazar.a@gmail.com>
2020-06-18 00:53:01 +0300

    Edit

--- Typescript/lesson21/task10/index.js
+++ Typescript/lesson21/task10/index.js
@@ -2,13 +2,11 @@ export const finishForm = () => {
     const form = document.querySelector('form');
     const loginInputElem = document.createElement('input');
     form.append(loginInputElem);
+
     loginInputElem.setAttribute('type', 'text');
     loginInputElem.setAttribute('name', 'login');
 
-    const passwordInputElem = document.createElement('input');
-    form.append(passwordInputElem);
-    passwordInputElem.setAttribute('type', 'password');
-    passwordInputElem.setAttribute('name', 'password');
+    document.querySelector('input[name="password"]').setAttribute('type', 'password');
 };
 
 finishForm();
