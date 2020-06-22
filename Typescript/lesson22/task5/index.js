dce37aabbb2a05fe14d7400d68efd15faea4ac62
Nazar <panchenko.nazar.a@gmail.com>
2020-06-22 18:27:52 +0300

    Edit

---
+++ Typescript/lesson22/task5/index.js
@@ -0,0 +1,6 @@
+const searchBtn = document.querySelector('.search__btn');
+const searchInput = document.querySelector('.search__input');
+
+searchBtn.addEventListener('click', () => {
+    console.log(searchInput.value);
+});
