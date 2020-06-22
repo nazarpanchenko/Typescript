8d371564e30dafc89129373a16be0b5b1414b117
Nazar <panchenko.nazar.a@gmail.com>
2020-06-22 18:49:08 +0300

    Edit

--- Typescript/lesson22/task6/index.js
+++ Typescript/lesson22/task6/index.js
@@ -3,11 +3,12 @@ let clicked = 0;
 
 const click = () => {
     ++clicked;
+
+    if (clicked === 1) {
+        btn.removeEventListener('click', click);
+    }
+
     console.log('clicked');
 };
 
 btn.addEventListener('click', click);
-
-if (clicked === 1) {
-    btn.removeEventListener('click', click);
-}
