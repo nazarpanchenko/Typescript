e41ed39fd66657ef11796df25f7f8be1c47df7a8
Nazar <panchenko.nazar.a@gmail.com>
2020-07-03 20:47:51 +0300

    Edit

--- Typescript/lesson23/task3/index.js
+++ Typescript/lesson23/task3/index.js
@@ -51,7 +51,7 @@ renderListItems(tasks);
 
 
 const onTaskStatusChange = event => {
-    if (!event.target.matches('input')) return;
+    if (!event.target.tagName == 'input') return;
 
     let taskId = +(event.target.closest('li').dataset.id);
 
