8ffd0a83efc034c2a4a1b38a1d5ea44122d1ffb9
Nazar <panchenko.nazar.a@gmail.com>
2020-07-06 22:31:12 +0300

    Edit

--- Typescript/lesson24/task2/index.js
+++ Typescript/lesson24/task2/index.js
@@ -1,12 +1,13 @@
-// const formatter = new Intl.DateTimeFormat('en', {
-//     timeZone : 'UTC',
-//     hour : '2-digit',
-//     minute : '2-digit',
-//     hour12 : false
-// });
+const formatter = new Intl.DateTimeFormat('en', {
+    timeZone : 'UTC',
+    hour : '2-digit',
+    minute : '2-digit',
+    hour12 : false
+});
 
 export function getGreenwichTime(date) {
     return new Date(Date.UTC(2020, 0, 6, 43));
+    return formatter.format(date);
 }
 
-getGreenwichTime(newDate());
+getGreenwichTime(new Date());
