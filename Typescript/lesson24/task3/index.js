9afbd2efcb9a8559897c8d523e56969b041f4e8c
Nazar <panchenko.nazar.a@gmail.com>
2020-07-16 22:21:20 +0300

    Edit

--- Typescript/lesson24/task3/index.js
+++ Typescript/lesson24/task3/index.js
@@ -1,23 +1,16 @@
-export function getDiff(startDate, endDate) {
-    const biggerDate = (endDate > startDate) ? 
-        endDate : (startDate > endDate) ? 
-        startDate : 
-        endDate;
+const seconds = 60;
+const minutes = seconds * 60;
+const hours = minutes * 60;
+const days = hours * 24;
 
-    const smallerDate = (startDate < endDate) ? 
-        startDate : (endDate < startDate) ? 
-        endDate : 
-        startDate;
-
-        const milliSeconds = Math.abs(biggerDate - smallerDate) / 1000;
-        const seconds = Math.floor(milliSeconds / 60);
-        const minutes = Math.floor(seconds / 60);
-        const hours = Math.floor(minutes / 60);
-        const days = Math.floor(hours / 24);
+export const getDiff = (startDate, endDate) => {
+        const difference = Math.abs(endDate - startDate);
+        
+        const daysLeft = Math.floor(difference / days);
     
         const timeDifference = '' + days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's';
     
         return timeDifference;
-}
+};
 
 getDiff(new Date('07/07/2020 20:22:39'), new Date('07/08/2020 20:22:39'));
