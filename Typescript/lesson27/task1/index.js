e19ac5f1f9955a1fbcbfee78d1c8f5c838a74d9b
Nazar <panchenko.nazar.a@gmail.com>
2020-07-24 15:48:46 +0300

    Edit

---
+++ Typescript/lesson27/task1/index.js
@@ -0,0 +1,19 @@
+export const getLocalStorageData = () => {
+    return Object.entries(localStorage)
+        .reduce((acc, [key, value]) => {
+            let newValue;
+
+            try {
+                newValue = JSON.parse(value);
+            } catch(e) {
+                newValue = value;
+            }
+
+            return {
+                ...acc,
+                [key] : newValue
+            }
+        }, {});
+};
+
+getLocalStorageData();
