e4dc6695cbf1799e386878e075e59f2fb8280081
Nazar <panchenko.nazar.a@gmail.com>
2020-07-27 08:50:42 +0300

    Ed

---
+++ Typescript/lesson28/task2/index.js
@@ -0,0 +1,23 @@
+const favorites = ['id-2'];
+const tree = {
+    id: 'id-1',
+    name: 'products',
+    nodes: [
+        {
+            id: 'id-2',
+            name: 'Food',
+            nodes: []
+        },
+    ],
+};
+const result = markFavorites(tree, favorites);
+
+const markFavorites = (tree, favorites) => {
+    const isFavorite = favorites.includes(tree.id);
+
+    return {
+        ...tree,
+        isFavorite,
+        nodes: tree.nodes.map(childeNode => markFavorites(childNode, favorites)),
+    };
+};
