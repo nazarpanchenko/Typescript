c464209866df55095abe434106a7b011b1830d07
Nazar <panchenko.nazar.a@gmail.com>
2020-07-29 18:13:45 +0300

    Edit

--- Typescript/lesson29/task3/index.js
+++ Typescript/lesson29/task3/index.js
@@ -1,9 +1,9 @@
 export const printing = () => {
-    console.log('1');
-    setTimeout(() => console.log('2'), 0);
-    console.log('3');
-    setTimeout(() => console.log('4'), 1000);
-    console.log('5');
+    console.log(1);
+    setTimeout(() => console.log(2), 0);
+    console.log(3);
+    setTimeout(() => console.log(4), 1000);
+    console.log(5);
 
 };
 
