914f914abe816be53e8ef789e8a9a4679a5f70bc
Nazar <panchenko.nazar.a@gmail.com>
2020-08-02 16:41:10 +0300

    e

---
+++ Typescript/lesson30/task1/index.js
@@ -0,0 +1,36 @@
+const addImageWithPromise = imgSrc => {
+    const p = new Promise((resolveCb, rejectCb) => {
+        const imgElem = document.createElement('img');
+        imgElem.setAttribute('alt', 'My Photo');
+        imgElem.src = imgSrc;
+        const containerElem = document.querySelector('.page');
+        containerElem.append(imgElem);
+
+        const onImageLoaded = () => {
+            const { width, height } = imgElem;
+            resolveCb({ width: 200, height: 100 });
+        };
+
+        imgElem.addEventListener('load', onImageLoaded);
+
+        imgElem.addEventListener('error', () => rejectCb(new Error('Image load is failed...')));
+    });
+
+    return p;
+};
+
+const imgSrc = 'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
+
+const result = addImageWithPromise(imgSrc);
+
+// const onImageLoaded = (error, imgElem) => {
+//     if (error) {
+//         console.log(error);
+//         return;
+//     }
+//     const { width, height } = imgElem;
+//     const sizeElem = document.querySelector('.image-size');
+//     sizeElem.textContent = `${width} x ${height}`;
+// };
+
+// addImage('https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg', onImageLoaded);
