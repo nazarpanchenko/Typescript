03e8a36ba110f02cef6462bd5ac96adff8fbaf06
Nazar <panchenko.nazar.a@gmail.com>
2020-08-11 22:42:24 +0300

    E

---
+++ Typescript/Lesson32/task3/index.js
@@ -0,0 +1,15 @@
+/*
+ * ф-ция makePromise должна вернуть промис со значением переданным в ф-цию
+ */
+
+/* ...code here */
+export const makePromise = number => new Promise(resolve => {
+    resolve(number);
+});
+/*
+ * пример использования
+ */
+makePromise(17)
+    .then(number => {
+        console.log(number); // 17
+    });
