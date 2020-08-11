151ea9aa3dd7d0f5fa673608b7b77dc7bf23c772
Nazar <panchenko.nazar.a@gmail.com>
2020-08-11 22:00:27 +0300

    E

---
+++ Typescript/Lesson32/task1/index.js
@@ -0,0 +1,24 @@
+const getValueWithDelay = (value, delay) => new Promise(resolve => {
+    setTimeout(() => {
+        console.log(value);
+        resolve(value);
+    }, delay);
+});
+
+const asyncNumber1 = getValueWithDelay(56, 1000);
+const asyncNumber2 = getValueWithDelay(4, 2000);
+
+const getSum = numbers => 
+    numbers
+        .filter(value => !isNaN(value))
+        .reduce((acc, num) => acc + Number(num), 0);
+
+export const asyncSum = (...asyncNumbers) => {
+    return Promise.all(asyncNumbers)
+        .then(numbers => getSum(numbers))
+        .catch(() => Promise.reject(new Error('Can\'t calculate!')));
+};
+
+asyncSum(asyncNumber1, Promise.reject(new Error('err')))
+    .then(result => console.log(result));
+
