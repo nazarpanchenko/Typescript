a1ebfa5821e53b69dac65a34f754385cdc64d918
Nazar <panchenko.nazar.a@gmail.com>
2020-08-12 21:04:48 +0300

    E

---
+++ Typescript/Lesson33/task2/index.js
@@ -0,0 +1,19 @@
+const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
+
+export const getTasksList = () => {
+    return fetch(baseUrl)
+        .then(response => response.json());
+};
+
+export const getTaskById = taskId => {
+    return fetch(baseUrl)
+        .then(response => response.json());
+};
+
+getTasksList().then(tasksList => {
+    console.log(tasksList);
+});
+
+getTaskById('2').then(taskData => {
+    console.log(taskData);
+});
