fadd43a02ca1160554a8e1e96897de18322214e8
Nazar <panchenko.nazar.a@gmail.com>
2020-08-12 19:27:58 +0300

    E

--- Typescript/Lesson33/task1/index.js
+++ Typescript/Lesson33/task1/index.js
@@ -1,12 +1,10 @@
-const userAvataElem = document.querySelector('.user__avatar');
+const userAvatarElem = document.querySelector('.user__avatar');
 const userNameElem = document.querySelector('.user__name');
 const userLocationElem = document.querySelector('.user__location');
 
 const defaultAvatar = 'https://avatars3.githubusercontent.com/u10001';
 
-userAvataElem.src = defaultAvatar;
-
-// https://api.github.com/users/USERNAME
+userAvatarElem.src = defaultAvatar;
 
 const showUserBtnElem = document.querySelector('.name-form__btn');
 const userNameInputElem = document.querySelector('.name-form__input');
@@ -18,7 +16,7 @@ const fetchUserData = userName => {
 
 const renderUserData = userData => {
     const { avatar_url, name, location } = userData;
-    userAvataElem.src = avatar_url;
+    userAvatarElem.src = avatar_url;
     userNameElem.textContent = name;
     userLocationElem.textContent = location
         ? `from${location}`
