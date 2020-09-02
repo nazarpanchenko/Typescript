2fc99a61156c34222436049413ac3400915f82c2
Nazar <panchenko.nazar.a@gmail.com>
2020-09-02 13:12:52 +0300

    E

--- Typescript/Lesson34/task3/index.js
+++ Typescript/Lesson34/task3/index.js
@@ -10,37 +10,26 @@ const formInput = document.querySelectorAll('.form-input');
 const submitBtn = document.querySelector('.submit-button');
 const errorText = document.querySelector('.error-text');
 const emailElem = document.querySelector('#email');
-const nameElem = document.querySelector('#name');
+const userNameElem = document.querySelector('#name');
 const passwordElem = document.querySelector('#password');
 
-const newUser = {
-    email : emailElem.textContent,
-    userName : nameElem.textContent,
-    password : passwordElem.textContent
-};
-const onEmailChange = event => {
-    emailElem.textContent = event.target.value;
-    newUser.email = emailElem.textContent;
-    errorText.textContent = '';
-};
-const onUserNameChange = event => {
-    nameElem.textContent = event.target.value;
-    newUser.userName = nameElem.textContent;
-    errorText.textContent = '';
-};
-const onPasswordChange = event => {
-    passwordElem.textContent = event.target.value;
-    newUser.password = passwordElem.textContent;
-    errorText.textContent = '';
+const user = {
+    email : '',
+    userName : '',
+    password : ''
 };
 
 const onCreateUser = () => {
+    user.email = emailElem.value;
+    uer.userName = userNameElem.value;
+    user.password = passwordElem.value;
+
     return fetch(baseUrl, {
         method: 'POST',
         headers: {
             'Content-Type' : 'application/json;charset=utf-8'
         },
-        body: JSON.stringify(newUser)
+        body: JSON.stringify(user)
     })
     .then(response => {
         if (!response.ok) {
@@ -54,6 +43,3 @@ const onCreateUser = () => {
 };
 
 submitBtn.addEventListener('click', onCreateUser);
-emailElem.addEventListener('change', onEmailChange);
-nameElem.addEventListener('change', onUserNameChange);
-passwordElem.addEventListener('change', onPasswordChange);
