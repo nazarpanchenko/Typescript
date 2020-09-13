79cc774c2cd2cf48621574bd4353eabb939bf4cc
Nazar <panchenko.nazar.a@gmail.com>
2020-09-13 15:58:39 +0300

    E

--- Typescript/Lesson34/task3/index.js
+++ Typescript/Lesson34/task3/index.js
@@ -2,6 +2,7 @@ const form = document.querySelector('form');
 const emailElem = document.querySelector('#email');
 const userNameElem = document.querySelector('#name');
 const passwordElem = document.querySelector('#password');
+let errorElem = document.querySelector('.error-text');
 
 const createUser = userData => {
     const baseUrl = 'https://5f4107e4a5e9db0016302376.mockapi.io/api/v1/allUsers';
@@ -21,15 +22,13 @@ const clearForm = () => {
 };
 
 const onError = () => {
-    let errorElem = document.querySelector('.error-text');
     errorElem.textContent = 'Failed to create user';
 };
 
 const onFormInputChange = () => {
     const submitBtn = document.querySelector('.submit-button');
     let isFormInputValid = form.reportValidity();
-    let errorElem = document.querySelector('.error-text');
-
+    
     if (isFormInputValid) {
         submitBtn.removeAttribute('disabled');
     } else {
@@ -39,15 +38,11 @@ const onFormInputChange = () => {
 };
 
 const onFormSubmit = () => {
-    let emailElem = document.querySelector('#email');
-    let userNameElem = document.querySelector('#name');
-    let passwordElem = document.querySelector('#password');
     const user = {
         email : emailElem.value,
         userName : userNameElem.value,
         password : passwordElem.value
     };
-
     createUser(user)
         .then(response => response.json())
         .then((responseData) => {
