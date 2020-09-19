25b377b8d68139370333e5094780f50707448e80
Nazar <panchenko.nazar.a@gmail.com>
2020-09-19 21:24:18 +0300

    E

--- Typescript/lesson35/task4/spinner.js
+++ Typescript/lesson35/task4/spinner.js
@@ -1,10 +1,10 @@
 const spinerElem = document.querySelector('.spinner');
 
 export const showSpinner = () => {
-    spinerElem.classList.remove('.spinner_hidden');
+    spinerElem.classList.remove('spinner_hidden');
 };
 
 export const hideSpinner = () => {
-    spinerElem.classList.add('.spinner_hidden');
+    spinerElem.classList.add('spinner_hidden');
 
 };
