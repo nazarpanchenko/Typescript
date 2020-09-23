8320457cf6345b927a5dae64412bce984c54b970
Nazar <panchenko.nazar.a@gmail.com>
2020-09-23 18:15:06 +0300

    E

--- Typescript/lesson36/task1/index.js
+++ Typescript/lesson36/task1/index.js
@@ -10,7 +10,3 @@ export const fetchUser = async userId => {
         throw new Error('Failed to fetch user');
     }
 };
-
-fetchUser('github')
-    .then(userData => console.log(userData))
-    .catch(err => alert(err.message));
