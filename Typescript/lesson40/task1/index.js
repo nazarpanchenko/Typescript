73bb03ee79df48d21bec414c904611ec9b780842
Nazar <panchenko.nazar.a@gmail.com>
2020-10-06 11:52:09 +0300

    e

--- Typescript/lesson40/task1/index.js
+++ Typescript/lesson40/task1/index.js
@@ -4,7 +4,7 @@
 // импортируйте ф-ции sum и mult под такими же именами, а так же ф-цию calc под именем calcExpression из файла calculator/index.js
 // импортируйте ф-цию fetchUser из profile/gateway.js под таким же именем
 // импортируйте ф-цию printProfile из profile/index.js под таким же именем
-import calcResult, { sum, mult } from './calculator/index.js';
+import calcExpression, { sum, mult } from './calculator/index.js';
 import fetchUser from './profile/gateway.js';
 import { printProfile } from './profile/index.js';
 import './polyfills/array-flat.js';
