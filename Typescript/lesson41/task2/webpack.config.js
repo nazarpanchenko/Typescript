ea21d67440643951be8dac169f07fcbea2acdd28
Nazar <panchenko.nazar.a@gmail.com>
2020-10-15 20:15:13 +0300

    e

--- Typescript/lesson41/task2/webpack.config.js
+++ Typescript/lesson41/task2/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry : {
         profile: './src/profile/index.js',
-        dashboard : './src/profile/index.js'
+        dashboard : './src/dashboard/index.js'
     },
     output : {
         filename: '[name].js',
