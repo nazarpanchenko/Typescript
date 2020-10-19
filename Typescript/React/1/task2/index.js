737ee96923ce4e4a03e6baa8757904df15bd9bfd
Nazar <panchenko.nazar.a@gmail.com>
2020-10-19 13:59:51 +0300

    e

--- Typescript/React/1/task2/index.js
+++ Typescript/React/1/task2/index.js
@@ -1,13 +1,20 @@
 //index.js
 
+const rootElement = document.querySelector('#root');
+
 const element = React.createElement(
     'div',
     { className : 'greeting' },
-    [
-        React.createElement(
-            'div',
-            { className : 'greeting__title' },
-            'Hello, World'
-        )
-    ]
+    React.createElement(
+        'div',
+        { className : 'greeting__title' },
+        'Hello, World'
+    ),
+    React.createElement(
+        'div',
+        { className : 'greeting__text' },
+        'I\'m learning React'
+    )
 );
+
+ReactDOM.render(element, rootElement);
