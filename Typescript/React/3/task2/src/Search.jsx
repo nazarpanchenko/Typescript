fbb142e52ba7833c8620e17469bb4e1d12aef4f2
Nazar <panchenko.nazar.a@gmail.com>
2020-10-20 18:24:18 +0300

    e

---
+++ Typescript/React/3/task2/src/Search.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import './search.scss'
+
+export default class Search extends Component {
+    render() {
+        return (
+            <div className='search'>
+            <h1 className="search__title">{`Hello, ${this.props.name}. What to search for you?`}</h1>
+            <div className="search__field">
+                <input type="text" className="search__input"/>
+                <button className="search__button">Search</button>
+            </div>
+            </div>
+        );
+    }
+}
