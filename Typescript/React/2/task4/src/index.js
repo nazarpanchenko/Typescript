0dd951d72ea0a84e529190aa5855f7689d45c628
Nazar <panchenko.nazar.a@gmail.com>
2020-10-20 15:11:17 +0300

    e

---
+++ Typescript/React/2/task4/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+
+const rootElement = document.querySelector('#root');
+
+const element = (
+    <div>{17}</div>
+);
+
+ReactDOM.render(element, rootElement);
