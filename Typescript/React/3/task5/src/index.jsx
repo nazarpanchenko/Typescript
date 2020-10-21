c5e09b0fd41905260930cbb0dc0731f7bae699af
Nazar <panchenko.nazar.a@gmail.com>
2020-10-21 16:35:33 +0300

    e

--- Typescript/React/3/task5/src/index.jsx
+++ Typescript/React/3/task5/src/index.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Profile from './Profile';
 
 const root = document.querySelector('#root');
 
+const userData = {
+    firstName: 'John',
+    lastName: 'Doe',
+    birthDate: '1991-01-17T11:11:11.819Z',
+    birthPlace: 'London'
+};
+
 ReactDOM.render(
-    <App />,
+    <Profile 
+        firstName={userData.firstName}
+        lastName={userData.lastName}
+        birthDate={userData.birthDate}
+        birthPlace={userData.birthPlace}
+    />,
     root
 );
