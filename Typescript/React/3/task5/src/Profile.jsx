1eceeb4a1bf476e5882d8797d358d2e9e04867f3
Nazar <panchenko.nazar.a@gmail.com>
2020-10-21 17:05:58 +0300

    E

--- Typescript/React/3/task5/src/Profile.jsx
+++ Typescript/React/3/task5/src/Profile.jsx
@@ -7,12 +7,12 @@ const formatDate = date => moment(date).format("DD MMM YY");
 function Profile(props) {
     return (
         <div className="profile">
-            <div className="profile__name">
-                {props.firstName} {props.lastName}
-            </div>
-            <div className="profile__birth">
+            <span className="profile__name">
+                {props.firstName} {props.lastName} 
+            </span>
+            <span className="profile__birth">
                 Was born {formatDate(props.birthDate)} in {props.birthPlace}
-            </div>
+            </span>
         </div>
     );
 };
