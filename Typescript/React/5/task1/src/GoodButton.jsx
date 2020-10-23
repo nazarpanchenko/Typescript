d1a9ba5344d7324496597a3f5fc0a817b0cb9981
Nazar <panchenko.nazar.a@gmail.com>
2020-10-23 16:13:18 +0300

    e

--- Typescript/React/5/task1/src/GoodButton.jsx
+++ Typescript/React/5/task1/src/GoodButton.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 const GoodButton = () => {
-    <button 
-        clasName="fancy-button"
-        onClick={() => alert('Good job!')}
-    >
-        Click me!
-    </button>
+    return (
+        <button 
+            className="fancy-button"
+            onClick={() => alert('Good job!')}
+        >
+            Click me!
+        </button>
+    );
 };
 
 export default GoodButton;
