d2073feab9efef388ce23f4cb7643adc05dbdcf4
Nazar <panchenko.nazar.a@gmail.com>
2020-10-23 16:25:15 +0300

    re

---
+++ Typescript/React/5/task2/src/GoodButton.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class GoodButton extends React.Component {
+    handleClick(e) {
+        
+        alert('Good job!');
+    }
+
+    render() {
+        return (
+            <button 
+                className="fancy-button"
+                onClick={() => this.handleClick}
+            >
+                Click me!
+            </button>
+        );
+    }
+}
+
+export default GoodButton;
