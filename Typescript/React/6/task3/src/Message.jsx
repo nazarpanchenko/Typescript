546eeeef04a6e67d0534242e1862951ce898c3e2
Nazar <panchenko.nazar.a@gmail.com>
2020-10-25 12:08:10 +0200

    E

---
+++ Typescript/React/6/task3/src/Message.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Message = ({ text }) => {
+    if (!text) return null;
+            
+    return (
+        <div className="message">
+            {text}
+        </div>
+    );
+}
+
+export default Message;
