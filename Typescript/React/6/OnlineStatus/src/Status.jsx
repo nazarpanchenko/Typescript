f46e876ab23c0b0732344994d1e7db086f3fd026
Nazar <panchenko.nazar.a@gmail.com>
2020-10-26 13:09:16 +0200

    E

--- Typescript/React/6/OnlineStatus/src/Status.jsx
+++ Typescript/React/6/OnlineStatus/src/Status.jsx
@@ -1,27 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Online from './Online';
 import Offline from './Offline';
 
-class Status extends Component {  
-    state = {
-        isOnline : false
-    };
-
-    render() {
-        let onlineStatus;
-
-        if (this.state.isOnline) {
-            onlineStatus = <Online />;
-        } else {
-            onlineStatus = <Offline />;
-        }
-
-        return (
-           <div className="status">
-               {onlineStatus}
-           </div>
-        );
-    }
+const Status = ({ isOnline }) => {
+    return (
+        <div className="status">
+            { isOnline ? <Online /> : <Offline /> }
+        </div>
+    );
 }
 
 export default Status;
