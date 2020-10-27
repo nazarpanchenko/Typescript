92fb8d305d7deb659020ddbae70cee0d0c390638
Nazar <panchenko.nazar.a@gmail.com>
2020-10-27 15:18:19 +0200

    e

--- Typescript/React/8/task1/src/Life.jsx
+++ Typescript/React/8/task1/src/Life.jsx
@@ -13,7 +13,7 @@ class Life extends Component {
     shouldComponentUpdate(nextProps, nextState) {
         console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
 
-        return nextProps.number % 2;
+        return nextProps.number;
     }
 
     componentDidUpdate(prevProps, prevState) {
