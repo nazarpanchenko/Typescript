12702fe680803f35cc121bcf899b2d0a672ae380
Nazar <panchenko.nazar.a@gmail.com>
2020-10-28 13:39:34 +0200

    e

---
+++ Typescript/React/8/ServerData/src/App.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import User from './User';
+
+class App extends Component {
+    render() {
+        return <User userId="facebook" />
+    }
+};
+
+export default App;
