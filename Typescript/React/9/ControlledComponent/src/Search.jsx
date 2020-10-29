85fc0a3f9b78c11c32b93b676b7700f8c16696a5
Nazar <panchenko.nazar.a@gmail.com>
2020-10-29 12:07:07 +0200

    E

---
+++ Typescript/React/9/ControlledComponent/src/Search.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class Search extends React.Component {
+    state = { value : '' };
+
+    handleChange = e => {
+        this.setState({ value : e.target.value });
+    }
+
+    search = e => {
+        e.preventDefault();
+        alert('Search text: ' + this.state.value);
+    }
+
+    render() {
+        return (
+            <form className="search" onSubmit={this.search}>
+                <input 
+                    type="text" 
+                    onChange={this.handleChange} 
+                    value={this.state.value} 
+                    className="search__input" 
+                />
+                <button type="submit" className="search__button">
+                    Search
+                </button>
+            </form>
+        );
+    }
+}
+
+export default Search;
---
+++ Typescript/React/9/ControlledComponent/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Search from './Search';
+
+const root = document.querySelector('#root');
+
+ReactDOM.render(<Search />, root);
