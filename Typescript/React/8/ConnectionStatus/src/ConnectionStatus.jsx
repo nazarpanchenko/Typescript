6176fe443160382b628e7c2378ecf4299f16285b
Nazar <panchenko.nazar.a@gmail.com>
2020-10-29 10:53:55 +0200

    ER

--- Typescript/React/8/ConnectionStatus/src/ConnectionStatus.jsx
+++ Typescript/React/8/ConnectionStatus/src/ConnectionStatus.jsx
@@ -10,7 +10,7 @@ class ConnectionStatus extends React.Component {
         window.addEventListener('offline', this.showConnectionStatus);
     }
 
-    componentWillUnmount() {
+    componentDidUpdate() {
         window.removeEventListener('online', this.showConnectionStatus);
         window.removeEventListener('offline', this.showConnectionStatus);
     }
@@ -19,7 +19,6 @@ class ConnectionStatus extends React.Component {
         this.setState({
             connected : navigator.onLine ? 'online' : 'offline'
         });
-        return this.state.connected;
     }
 
     render() {
