ee754cb01e1036c08df19854748f615eff939f0f
Nazar <panchenko.nazar.a@gmail.com>
2020-11-01 18:01:17 +0200

    E

---
+++ Typescript/React/10/WaterCalculator/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Calculator from './Calculator';
+
+const root = document.querySelector('#root');
+
+ReactDOM.render(<Calculator />, root);
