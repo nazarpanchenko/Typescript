4a3249edb52abf24f16f5db5561d75f927331699
Nazar <panchenko.nazar.a@gmail.com>
2020-11-02 15:11:33 +0200

    E

--- Typescript/React/11/ExpandedComponent/src/Expand.jsx
+++ Typescript/React/11/ExpandedComponent/src/Expand.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Expand = ({ isHidden, title, children, handleToggle }) => {
+    const toggleArrow = isHidden ? 'fas fa-chevron-down' : 'fas fa-chevron-up',
+        toggleContent = isHidden ? null : children;
+
     return (
         <div className="expand border">
             <div className="expand__header">
                 <span className="expand__title">{title}</span>
                 <button className="expand__toggle-btn" onClick={handleToggle}>
-                    <i className="fas fa-chevron-up"></i>
+                   <i className={toggleArrow}></i>
                 </button>
             </div>
-            <div className="expand__content"> { !isHidden ? null : children }</div>
+            <div className="expand__content">{toggleContent}</div>
         </div>
     );
 }
