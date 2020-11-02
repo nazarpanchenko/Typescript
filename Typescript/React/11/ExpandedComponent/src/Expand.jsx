1a09b2b90988b3a1e7198a72174ba0a9abaf2a25
Nazar <panchenko.nazar.a@gmail.com>
2020-11-02 14:49:19 +0200

    E

--- Typescript/React/11/ExpandedComponent/src/Expand.jsx
+++ Typescript/React/11/ExpandedComponent/src/Expand.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import PropTypes from 'prop-types';
 
 const Expand = ({ isHidden, title, children, handleToggle }) => {
@@ -9,7 +7,7 @@ const Expand = ({ isHidden, title, children, handleToggle }) => {
             <div className="expand__header">
                 <span className="expand__title">{title}</span>
                 <button className="expand__toggle-btn" onClick={handleToggle}>
-                   <FontAwesomeIcon icon={isHidden ? faChevronUp : faChevronDown} /> 
+                    <i class="fas fa-chevron-up"></i>
                 </button>
             </div>
             <div className="expand__content"> { !isHidden ? null : children }</div>
