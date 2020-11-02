c03932691aed9d955c5b794b82b9f57e9cc45ecb
Nazar <panchenko.nazar.a@gmail.com>
2020-11-02 20:35:19 +0200

    E

--- Typescript/React/11/ExpandedComponent/src/App.jsx
+++ Typescript/React/11/ExpandedComponent/src/App.jsx
@@ -6,12 +6,19 @@ class App extends Component {
         display : false
     };
 
+    handleDisplay = () => {
+        this.setState({
+            display : !this.state.display
+        });
+    }
+
     render() {
         return (
             <div className="app">
                 <Expand 
-                    title='Some title'
-                    isShown={this.state.display}>
+                    isShown={this.state.display}
+                    title='Some title' 
+                    handleDisplay={this.handleDisplay}>
                         <p>Hooks are a new addition in React 16.8. They let you use state and other React features without writing a class.</p>
                 </Expand>
             </div>
--- Typescript/React/11/ExpandedComponent/src/Expand.jsx
+++ Typescript/React/11/ExpandedComponent/src/Expand.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Expand = ({ title, children, isShown }) => {
+const Expand = ({ isShown, title, children, handleDisplay }) => {
+    const ARROW = !isShown ? "fas fa-chevron-down" : "fas fa-chevron-up";
+
     return (
         <div className="expand border">
             <div className="expand__header">
                 <span className="expand__title">{title}</span>
-                <button className="expand__toggle-btn">
-                   <i className="fas fa-chevron-up"></i>
+                <button className="expand__toggle-btn" onClick={handleDisplay}>
+                   <i className={ARROW}></i>
                 </button>
             </div>
-            <div className="expand__content">{ !isShown ? null : children }</div>
+            <div className="expand__content">{!isShown ? null : children}</div>
         </div>
     );
 }
+
+Expand.propTypes = {
+    isHidden: PropTypes.bool,
+    title: PropTypes.string,
+    children: PropTypes.element.isRequired,
+    handleDisplay: PropTypes.func.isRequired
+};
+
+Expand.defaultProps = {
+    title: '',
+    isShown: false
+};
 
 export default Expand;
