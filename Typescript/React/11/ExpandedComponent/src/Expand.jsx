8f67d84edfbf7b8c6e818fed3be0c43bd970fef5
Nazar <panchenko.nazar.a@gmail.com>
2020-11-03 10:37:28 +0200

    e

--- Typescript/React/11/ExpandedComponent/src/Expand.jsx
+++ Typescript/React/11/ExpandedComponent/src/Expand.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Expand = ({ isShown, title, children, handleDisplay }) => {
-    const ARROW = !isShown ? "fas fa-chevron-down" : "fas fa-chevron-up";
-
+    const ARROW_ICON = !isShown ? "fas fa-chevron-down" : "fas fa-chevron-up";
     return (
         <div className="expand border">
             <div className="expand__header">
                 <span className="expand__title">{title}</span>
                 <button className="expand__toggle-btn" onClick={handleDisplay}>
-                   <i className={ARROW} />
+                   <i className={ARROW_ICON} />
                 </button>
             </div>
             <div className="expand__content">{ !isShown ? null : children }</div>
@@ -19,13 +18,11 @@ const Expand = ({ isShown, title, children, handleDisplay }) => {
 
 Expand.propTypes = {
     isShown: PropTypes.bool,
-    title: PropTypes.string,
+    title: PropTypes.string.isRequired,
     children: PropTypes.element.isRequired,
     handleDisplay: PropTypes.func.isRequired
 };
-
 Expand.defaultProps = {
-    title: '',
     isShown: false
 };
 
