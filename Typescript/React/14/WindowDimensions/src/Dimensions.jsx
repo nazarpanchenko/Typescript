243a74f3482c8b0f2c8d79b0d336de656117d977
Nazar <panchenko.nazar.a@gmail.com>
2020-11-06 17:41:17 +0200

    E

---
+++ Typescript/React/14/WindowDimensions/src/Dimensions.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+
+const Dimensions = () => {
+    const [dimensions, setDimensions] = useState({
+        width: null,
+        height: null
+    });
+    const { width, height } = dimensions;
+
+    useEffect(() => {
+        const { innerWidth, innerHeight } = window;
+        setDimensions({ width: innerWidth, height: innerHeight });
+
+        const handleResize = e => {
+            const { innerWidth, innerHeight } = e.target;
+            setDimensions({ width: innerWidth, height : innerHeight });
+        };
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        }
+    }, []);
+
+    return <div className="dimensions">{`${width}px - ${height}px`}</div>;
+}
+
+export default Dimensions;
