ecf886b99e82b28876072e7bb1a30874baed3ab8
Nazar <panchenko.nazar.a@gmail.com>
2020-11-06 19:07:06 +0200

    E

---
+++ Typescript/React/14/OnlineOffline/src/ConnectionStatus.jsx
@@ -0,0 +1,40 @@
+import React, { Component, useState } from 'react';
+import classNames from 'classnames';
+
+class ConnectionStatus extends Component {
+    state = {
+        isOnline : 'online'
+    };
+
+    componentDidMount() {
+        window.addEventListener('online', this.handleOnineStatus);
+        window.addEventListener('offline', this.handleOffineStatus);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('online', this.handleOnineStatus);
+        window.removeEventListener('offline', this.handleOffineStatus);
+    }
+
+    handleOnlineStatus = () => {
+        this.setState({ isOnline : 'online' });
+    }
+
+    handleOfflineStatus = () => {
+        this.setState({ isOnline : 'offline' });
+    }
+
+    render() {
+        const statusText = (this.state.isOnline === 'online') ? 'Online' 
+            : (this.state.isOnline === 'offline') ? 'Offline'
+            : 'online';
+
+        return (
+            <div className={classNames('status', { 'status_offline' : !this.state.isOnline })}>
+                {statusText}
+            </div>
+        );
+    }
+}
+
+export default ConnectionStatus;
