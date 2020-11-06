f98d8c8d2257c000720b4c6b2c149e630a936bbd
Nazar <panchenko.nazar.a@gmail.com>
2020-11-06 15:49:15 +0200

    E

---
+++ Typescript/React/14/Counter/webpack.config.js
@@ -0,0 +1,60 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require("webpack");
+const path = require('path');
+
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === "production";
+    const config = {
+        entry: "./src/index.jsx",
+        output: {
+            filename: "bundle.js",
+            publicPath: '/'
+        },
+        module: {
+            rules: [
+                {
+                    test: /.jsx?$/,
+                    use: ["babel-loader"]
+                },
+                {
+                    test: /.s?css$/,
+                    use: [
+                        isProduction
+                            ? MiniCssExtractPlugin.loader
+                            : "style-loader",
+                        "css-loader",
+                        "sass-loader",
+                    ]
+                }
+            ],
+        },
+        plugins: [
+            new webpack.ProgressPlugin(),
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                template: "./src/index.html"
+            }),
+        ],
+        resolve: {
+            extensions: [".js", ".jsx"]
+        },
+        devServer: {
+            hot: true,
+            port: 9007,
+            historyApiFallback: true
+        },
+        output : {
+            path : path.resolve(__dirname, 'review_build')
+        }
+    };
+
+    if (isProduction) {
+        config.plugins.push(new MiniCssExtractPlugin({
+            filename: "[name].css",
+        }));
+    }
+
+    return config;
+};
